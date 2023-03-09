498dc4ce95e04d3885628c94b2fc32f8146e1321
Evans Rutto <robbyevans001@gmail.com>
2023-03-09 10:58:50 +0300

    done

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -11,18 +11,27 @@ function Featured() {
       <div className={css.body}>
         <div className={css.card}>
           <img src='./Rectangle 4.png'/>
-          <span className={css.secondaryText}>The Palace</span>
-          <span>Karen,the place</span>
+          <div className={css.cardWrap}>
+            <span className={css.secondaryText}>The Palace</span>
+            <span>Karen,the place</span>
+            <span className={css.left}>Rent: Ksh 35000</span>
+          </div>
         </div>
         <div className={css.card}>
         <img src='./Rectangle 4 (1).png'/>
-          <span className={css.secondaryText}>Ivy Rory</span>
-          <span>Kilimani,phase 3</span>
+          <div className={css.cardWrap}>
+            <span className={css.secondaryText}>Ivy Rory</span>
+            <span>Kilimani,phase 3</span>
+            <span className={css.left}>Rent: Ksh 20000</span>
+          </div>
         </div>
         <div className={css.card}>
         <img src='./Rectangle 4 (2).png'/>
-          <span className={css.secondaryText} >Rexona</span>
-          <span>Ngong,phase 1</span>
+          <div className={css.cardWrap}>
+            <span className={css.secondaryText} >Rexona</span>
+            <span>Ngong,phase 1<br/></span>
+            <span className={css.left}>Rent: Ksh 55000</span>
+          </div>
         </div>
       </div>
     </div>
